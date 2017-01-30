72e53092103846bf935e66eb199a84eda55f65d6
Phil Boothe <pfb3cn@virginia.edu>
2017-01-30 15:47:08 -0500

    new car form with state.newCar object

--- src/js/app.js
+++ src/js/app.js
@@ -27,8 +27,26 @@ const MCM = {
 const cars = [oldBlue, mercy, MCM];
 
 // class ColorList extends React.Component {
-//     render() {
 //
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             newColor: ''
+//         };
+//
+//         // Binds the instance object to the prototype method,
+//         // creating a new onChange method for each instance
+//         this.onChange = this.onChange.bind(this);
+//     }
+//
+//     onChange(e) {
+//         this.setState({
+//             [e.currentTarget.name]: e.currentTarget.value
+//         });
+//     }
+//
+//     render() {
 //         return (
 //             <div>
 //                 <h1>Color List</h1>
@@ -37,12 +55,42 @@ const cars = [oldBlue, mercy, MCM];
 //                         <li>{myColor}</li>
 //                     )}
 //                 </ul>
+//                 <form>
+//                     <div>
+//                         <label htmlFor="new-color-input">New Color</label>
+//                         <input type="text" id="new-color-input" name="newColor"
+//                             value={this.state.newColor} onChange={this.onChange} />
+//                     </div>
+//                 </form>
 //             </div>
-//     );
+//         );
 //     }
 // }
 
 class CarTool extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newCar: {
+                make: '',
+                model: '',
+                year: 2017,
+                color: '',
+                price: 0
+            }
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            newCar: Object.assign({}, this.state.newCar, { [e.currentTarget.name]: e.currentTarget.value })
+        });
+    }
+
     render() {
         return (
             <div>
@@ -69,10 +117,39 @@ class CarTool extends React.Component {
                         )}
                     </tbody>
                 </table>
+                <h3>Add New Car</h3>
+                <form>
+                    <div>
+                        <label htmlFor="new-make-input">Make</label>
+                        <input type="text" id="new-make-input" name="make"
+                        value={this.state.newCar.make} onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="new-model-input">Model</label>
+                        <input type="text" id="new-model-input" name="model"
+                        value={this.state.newCar.model} onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="new-year-input">Year</label>
+                        <input type="number" id="new-year-input" name="year"
+                        value={this.state.newCar.year} onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="new-color-input">Color</label>
+                        <input type="text" id="new-color-input" name="color"
+                        value={this.state.newCar.color} onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="new-price-input">Price</label>
+                        <input type="number" id="new-price-input" name="price"
+                        value={this.state.newCar.price} onChange={this.onChange} />
+                    </div>
+                </form>
             </div>
         );
     }
 }
 
 // ReactDOM.render(<ColorList myColors={colors} />, document.querySelector('main'));
+
 ReactDOM.render(<CarTool myCars={cars} />, document.querySelector('main'));
